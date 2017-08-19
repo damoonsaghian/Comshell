d8d62614f7f7c632b6a05c67ff15a105fe7270eb
Damoon Saghian <damoonsaghian84@gmail.com>
2017-08-19 09:45:30 +0430

    Add files via upload

--- main.rs
+++ main.rs
@@ -1,11 +1,9 @@
 extern crate futures;
 extern crate futures_cpupool;
-extern crate qt_gui;
 
 use std::path::Path;
 use futures::Future;
 use futures_cpupool::CpuPool;
-use qt_gui::*;
 
 fn main() {
 }
