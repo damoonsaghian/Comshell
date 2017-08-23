f68f93bd38041ed88cb75c8247dd2cdf665e5fc8
Damoon Saghian <damoonsaghian84@gmail.com>
2017-08-23 18:54:04 +0430

    Add files via upload

--- cargo/build.rs
+++ cargo/build.rs
@@ -5,11 +5,11 @@ an alternative way would be to add these to .cargo/config:
 
 [target.x86_64-pc-windows-gnu.clutter]
 rustc-link-search = ["c:\\msys64\\mingw64\\lib"]
-rustc-link-lib = ["evas"]
+rustc-link-lib = ["clutter"]
 
 [target.i686-pc-windows-gnu.clutter]
 rustc-link-search = ["c:\\msys32\\mingw32\\lib"]
-rustc-link-lib = ["evas"]
+rustc-link-lib = ["clutter"]
 */
 
 use std::path::PathBuf;
@@ -18,9 +18,9 @@ use std::env;
 fn main(){
   let target = env::var("TARGET").unwrap();
   if target.contains("pc-windows-gnu") {
-    let efl_lib_dir = PathBuf::from(
+    let lib_dir = PathBuf::from(
       if target.contains("x86_64") {r"C:\msys64\lib"} else {r"C:\msys32\lib"}
     );
-    println!("cargo:rustc-link-search=all={}", efl_lib_dir.display());
+    println!("cargo:rustc-link-search=all={}", lib_dir.display());
   }
 }
