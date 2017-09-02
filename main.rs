fef751bafb99d00fe53eeba378f3714eab7c6de2
Damoon Saghian <damoonsaghian84@gmail.com>
2017-09-02 19:16:22 +0430

    Add files via upload

--- main.rs
+++ main.rs
@@ -1,9 +1,15 @@
 extern crate futures;
 extern crate futures_cpupool;
+extern crate gfx_window_glutin;
+extern crate graphics;
+extern crate gfx_graphics;
 
 use std::path::Path;
 use futures::Future;
 use futures_cpupool::CpuPool;
+use gfx_window_glutin;
+use graphics::*;
+use gfx_graphics::Gfx2d;
 
 fn main() {
 }
