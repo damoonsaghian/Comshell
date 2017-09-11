d54b909d8ea53646b58d4e5c36d90a7b0d251c39
Damoon Saghian <damoonsaghian84@gmail.com>
2017-09-12 01:48:17 +0430

    Add files via upload

--- main.rs
+++ main.rs
@@ -1,9 +1,12 @@
 extern crate futures;
 extern crate futures_cpupool;
+extern crate sfml;
 
 use std::path::Path;
 use futures::Future;
 use futures_cpupool::CpuPool;
+use sfml::window;
+use sfml::graphics::*;
 
 fn main() {
 }
