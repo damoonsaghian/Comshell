910c8057450d7db5d83d56ec411653d3f6c71041
Damoon Saghian <damoonsaghian84@gmail.com>
2017-10-02 15:11:49 +0330

    Add files via upload

--- main.rs
+++ main.rs
@@ -1,9 +1,11 @@
 extern crate futures;
 extern crate futures_cpupool;
+extern crate font_rs;
 
 use std::path::Path;
 use futures::Future;
 use futures_cpupool::CpuPool;
+use font_rs::{raster, geom, font};
 
 fn main() {
 }
