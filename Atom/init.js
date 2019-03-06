a1ac4b6feaf24a9aba6f5966564ac14fa42ea9cd
Damoon Saghian <damoonsaghian84@gmail.com>
2019-03-06 19:22:39 +0330

    Add files via upload

--- Atom/init.js
+++ Atom/init.js
@@ -1,59 +1,107 @@
 // one pane per project
-// in center workspace, only the active pane is displayed, others will be hidden,
-//   by setting their CSS display property, to none:
+// in center workspace, only the active pane is displayed, others will be hidden;
 // panes are stored/restored to/form a file in the ".cache" directory of each project;
 
-// projects list:
-// https://github.com/chocoelho/project-switcher2/blob/master/lib/utils.coffee
+// hiding panes is done by setting their CSS display property of inactive panes, to none;
+// this method can be problematic;
+// eg when using the project mechanism of Atom the initial pane will not have "active" property,
+//   until another pane is added;
+// since we do not use project mechanism, we can ignore this;
+// nonetheless here is an alternative method:
+//   
+
 const fs = require("fs");
 const SelectList = require("atom-select-list");
-const projectsList = new SelectList({ items: [] });
-// if "~/projects/" directory does not exist, create it;
-fs.readdir("~/projects/", (err, paths) => {
-  if (err) { alert(err.message); }
-  else {
-    let projectPaths = paths.filter(paths =>
-                                    path[0] != '.' &&
-                                    fs.existsSync("~/projects/" + file) &&
-                                    fs.statSync("~/projects/" + file).isDirectory());
-    // set items, and populate list
-  }
-});
-// projectsList.element
-// on confirmed:
-// , change root dir in the tree_view,
-// , if there is a pane named "project_name" switch to it, otherwise create it, and focus tree_view
+
+// { "project name": projectPane }
+var projectPanes = {};
+
+const projectsList = {
+  selectList: new SelectList({
+    items: [],
+
+    didConfirmSelection: (item) => {
+      // item is actually a project's name;
+      this.selectList.reset();
+      // change root dir in the tree_view;
+      
+      if (!(item in projectPans)) {
+        let newPane = atom.workspace.getCenter().getActivePane().splitRight();
+        projectPanes[item] = newPane;
+        // focus tree_view
+      }
+      // hide all panes, show only the the selected project pane, and activate it;
+    },
+
+    didCancelSelection: () => {
+      this.selectList.reset();
+      this.modalPanel.hide();
+    },
+    
+    elementForItem: (item) => {
+      const li = document.createElement('li');
+      const span = document.createElement('span');
+      span.textContent = item;
+      li.appendChild(span);
+      return li;
+    }
+  }),
+
+  modalPanel: null,
+
+  show() {
+    // if "~/projects/" directory does not exist, create it;
+    fs.readdir("~/projects/", (err, fileNames) => {
+      if (err) { alert(err.message); }
+      else {
+        let projectNames = fileNames.filter((fileName) =>
+                                            fileName[0] != '.' &&
+                                            fs.statSync("~/projects/" + fileName).isDirectory());
+        this.selectList.update({ items: projectNames });
+      }
+    });
+        
+    if (!this.panel) {
+      this.modalPanel = atom.workspace.addModalPanel({ item: this.selectList , autoFocus: true });
+    }
+    this.panel.show();
+    //this.selectList.focus();
+  },
+
+  createNewProject() {}
+};
+
+// to define keybindings for projectsList, add a class:
+projectsList.selectList.element.classList.add('projects-list');
 
 // store/restore workspace to/from a config field:
 // https://github.com/denieler/save-workspace-atom-plugin/blob/master/lib/models/workspace.js
 
-// https://atom.io/packages/tree-view-git-status
+// https://atom.io/packages/tree-view-auto-collapse
 // https://atom.io/packages/tree-view-scope-lines
 // https://atom.io/packages/tree-lines
-// https://atom.io/packages/tree-view-auto-collapse
 // https://github.com/hswolff/tree-view-extended/tree/master/lib
+// https://atom.io/packages/tree-view-git-status
 // https://atom.io/packages/file-icons
+
+function getFileExtension(filename) {
+  return filename.slice((filename.lastIndexOf('.') - 1 >>> 0) + 2);
+}
+
+function getFileName(filename) {
+  return filename.slice(0, (filename.lastIndexOf('.') - 1 >>> 0) + 1);
+}
 
 // https://github.com/alexfu/atom-replace-pane
 // https://atom.io/packages/atom-clock
 // https://github.com/atom/settings-view/blob/master/lib/package-manager.coffee
+// https://atom.io/packages/structure-view
 // https://atom.io/packages/simple-git
 // https://atom.io/packages/git-plus
 
 // https://medium.com/hacking-atom/tweak-your-atom-s-init-script-without-reloading-atom-with-a-declarative-module-8b1c0f208663
 
-const workspaceCenter = atom.workspace.getCenter();
-
-function gotoProject(uri) {
-  let pane = workspaceCenter.getPanes().find(item => {
-    return item.getUri
-  });
-  if (!pane) {
-    workspaceCenter().getActivePane().splitRight();
-    // open uri
-  }
-}
-
+// 2 spaces -> enter
 atom.commands.add('atom-text-editor', 'custom:space', () => {
   const editor = atom.workspace.getActiveTextEditor();
   const cursor = editor.getLastCursor();
@@ -68,14 +116,7 @@ atom.commands.add('atom-text-editor', 'custom:space', () => {
   }
 })
 
-function getFileExtension(filename) {
-  return filename.slice((filename.lastIndexOf('.') - 1 >>> 0) + 2);
-}
-
-function getFileName(filename) {
-  return filename.slice(0, (filename.lastIndexOf('.') - 1 >>> 0) + 1);
-}
-
+/* importing from a package (probably a bad idea)
 atom.packages.activatePackage("tree-view").then((pkg) => {
   if (pkg && pkg.mainModule && pkg.mainModule.treeView) {
     treeView = pkg.mainModule.treeView;
@@ -91,3 +132,4 @@ atom.packages.activatePackage("tree-view").then((pkg) => {
     });
   }
 );
+*/
