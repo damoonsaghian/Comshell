408bce4f80e6efaf26eac9152b834ac96ff04ca2
Damoon Saghian <damoonsaghian84@gmail.com>
2019-05-26 00:29:09 +0430

    Add files via upload

---
+++ Atom/status-bar.js
@@ -0,0 +1,42 @@
+if (atom.packages.isPackageActive('status-bar')) {
+  const statusBar = atom.packages.getActivePackage('status-bar').mainModule.statusBar;
+  if (statusBar) { showDateTime(statusBar) }
+}
+atom.packages.onDidActivatePackage(activatedPackage => {
+  if (activatedPackage === atom.packages.getActivePackage('status-bar')) {
+    const statusBar = activatedPackage.mainModule.statusBar;
+    if (statusBar) { showDateTime(statusBar) }
+  }
+});
+
+function showDateTime(statusBar) {
+  const dateTimeElement = document.createElement('div');
+  dateTimeElement.classList.add('inline-block');
+  statusBar.addRightTile({ item: dateTimeElement, priority: -1 });
+
+  function updateDateTime() {
+    const now = new Date();
+    setTimeout(updateDateTime, (60 - now.getSeconds()) * 1000);
+    /*
+    const weekDay = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'][now.getDay()];
+    const hour = now.getHours();
+    const hour12 = hour == 0 ? 12 : hour > 12 ? hour - 12 : hour;
+    const minute = now.getMinutes();
+    const minute2digits = minute > 9 ? minute : '0' + minute;
+    const dayPeriod = hour > 11 ? 'PM' : 'AM';
+    dateTimeElement.textContent =
+      now.getFullYear() +'/'+ (now.getMonth()+1) +'/'+ now.getDate() +' '+
+      weekDay +' '+ dayPeriod +' '+ hour12 +':'+ minute2digits;
+    */
+
+    new (require('atom').BufferedProcess)({command: 'date', args: ['+"%F %a %p %I:%M"'],
+      stdout: output => dateTimeElement.textContent = output.slice(0,output.length - 2).slice(1)
+    });
+  }
+
+  // update date after computer wakes up from sleep;
+  require('electron').remote.powerMonitor.on('resume', () => updateDateTime())
+  updateDateTime();
+}
+
+// https://github.com/sebhildebrandt/systeminformation
