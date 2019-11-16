1f44b602b0947f3de6c0cdd1919967faf4c272ab
Damoon Saghian <damoonsaghian84@gmail.com>
2019-11-16 19:33:20 +0330

    Add files via upload

--- etc/arch-install.sh
+++ etc/arch-install.sh
@@ -2,15 +2,16 @@ pacman -S grub intel-ucode amd-ucode dracut linux linux-firmware \
   man-db nano unzip btrfs-progs e2fsprogs dosfstools udisks2 pulseaudio-alsa networkmanager \
   gdm sakura gvfs materia-gtk-theme ttf-hack noto-fonts
 
-printf "GRUB_TIMEOUT=0\nGRUB_DISABLE_OS_PROBER=true" > /etc/default/grub
-printf "set superusers=\"\"" > /etc/grub.d/40_custom
-printf "CLASS=\"--class gnu-linux --class gnu --class os --unrestricted\"" > /etc/grub.d/10_linux
+printf '\nGRUB_TIMEOUT=0\nGRUB_DISABLE_OS_PROBER=true\n' >> /etc/default/grub
+printf '\nset superusers=""\n' >> /etc/grub.d/40_custom
+printf '\nCLASS="--class gnu-linux --class gnu --class os --unrestricted"\n' >
+  /etc/grub.d/10_linux
 grub-mkconfig -o /boot/grub/grub.cfg
-grub-mkstandalone -O x86_64-efi -o "/boot/efi/EFI/BOOT/BOOTX64.EFI" \
-  "boot/grub/grub.cfg=/boot/grub/grub.cfg"
+grub-mkstandalone -O x86_64-efi -o '/boot/efi/EFI/BOOT/BOOTX64.EFI' \
+  'boot/grub/grub.cfg=/boot/grub/grub.cfg'
 # automatically update Grub every time "grub" package is upgraded:
 mkdir -p /etc/pacman.d/hooks
-printf "[Trigger]
+echo '[Trigger]
 Type = Package
 Operation = Upgrade
 Target = grub
@@ -18,15 +19,15 @@ Target = grub
 Description = Updating grub
 When = PostTransaction
 Exec = /usr/bin/grub-mkstandalone -O x86_64-efi -o \"/boot/efi/EFI/BOOT/BOOTX64.EFI\" \"boot/grub/grub.cfg=/boot/grub/grub.cfg\"
-" > /etc/pacman.d/hooks/100-grub.hook
+' > /etc/pacman.d/hooks/100-grub.hook
 
-printf "\nen_US.UTF-8 UTF-8" >> /etc/locale.gen
+printf '\nen_US.UTF-8 UTF-8\n' >> /etc/locale.gen
 locale-gen
-printf "LANG=en_US.UTF-8" > /etc/locale.conf
+printf 'LANG=en_US.UTF-8\n' > /etc/locale.conf
 
 # automatic time_zone:
 #!/bin/sh
-printf 'if [ "$2" = "connectivity-change" ] && [ -z "$VPN_IP_IFACE" ]; then
+echo 'if [ "$2" = "connectivity-change" ] && [ -z "$VPN_IP_IFACE" ]; then
   timedatectl set-timezone "$(curl --fail http://ip-api.com/line/?fields=timezone)"
 fi' > /etc/NetworkManager/dispatcher.d/09-timezone
 chmod 755 /etc/NetworkManager/dispatcher.d/09-timezone
@@ -63,10 +64,10 @@ passwd
 
 systemctl enable gdm
 # automatic login:
-printf "[daemon]
+echo '[daemon]
 AutomaticLogin=user1
 AutomaticLoginEnable=True
-" > /etc/gdm/custom.conf
+' > /etc/gdm/custom.conf
 
 echo '<?xml version="1.0"?>
 <!DOCTYPE fontconfig SYSTEM "fonts.dtd">
@@ -106,5 +107,10 @@ lock-enabled = 'false'
 " > /etc/dconf/db/local.d/1
 dconf update
 
-mkdir -p /usr/local/share/gnome-shell/extensions
-cp -r etc/gnome-shell-improved/ /usr/local/share/gnome-shell/extensions/
+mkdir -p /usr/local/share/gnome-shell/extensions/gnome-shell-improved/
+echo '{
+  "uuid": "gnome-shell-improved"
+}' > /usr/local/share/gnome-shell/extensions/gnome-shell-improved/metadata.json
+curl --proto '=https' -sSf \
+  https://github.com/damoonsaghian/Comshell/raw/master/etc/extension.js >
+  /usr/local/share/gnome-shell/extensions/gnome-shell-improved/
