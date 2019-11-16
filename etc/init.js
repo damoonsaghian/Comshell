211a5ddf2dabf9180af58474eeb6d2558196d53d
Damoon Saghian <damoonsaghian84@gmail.com>
2019-11-16 12:55:01 +0330

    Add files via upload

---
+++ etc/init.js
@@ -0,0 +1,10 @@
+const fs = require('fs');
+const path = require('path');
+
+atom.workspace.observeTextEditors(editor => {
+  const grammar = editor.getGrammar();
+  if (grammar.name === "Plain Text" || grammar === atom.grammars.nullGrammar)
+    atom.textEditors.setGrammarOverride(editor, 'text.plain');
+});
+
+require('./tree-view');
