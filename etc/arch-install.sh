df84c2db6c4217d796c64f765e40e47f64122658
Damoon Saghian <damoonsaghian84@gmail.com>
2020-02-06 23:00:00 +0330

    Add files via upload

--- etc/arch-install.sh
+++ etc/arch-install.sh
@@ -1,6 +1,7 @@
 pacman -S grub intel-ucode amd-ucode linux linux-firmware \
   btrfs-progs e2fsprogs dosfstools udisks2 pulseaudio-alsa networkmanager \
-  nano man-db unzip gdm gvfs materia-gtk-theme ttf-hack noto-fonts
+  nano man-db unzip gvfs materia-gtk-theme ttf-hack noto-fonts \
+  lightdm-gtk-greeter xorg-server
 
 printf '\nGRUB_TIMEOUT=0\nGRUB_DISABLE_OS_PROBER=true\n' >> /etc/default/grub
 printf '\nset superusers=""\n' >> /etc/grub.d/40_custom
@@ -25,16 +26,9 @@ printf '\nen_US.UTF-8 UTF-8\n' >> /etc/locale.gen
 locale-gen
 printf 'LANG=en_US.UTF-8\n' > /etc/locale.conf
 
-# automatic time_zone:
-#!/bin/sh
-echo 'if [ "$2" = "connectivity-change" ] && [ -z "$VPN_IP_IFACE" ]; then
-  timedatectl set-timezone "$(curl --fail http://ip-api.com/line/?fields=timezone)"
-fi
-' > /etc/NetworkManager/dispatcher.d/09-timezone
-chmod 755 /etc/NetworkManager/dispatcher.d/09-timezone
-
 systemctl enable systemd-timesyncd
 systemctl enable NetworkManager
+systemctl enable lightdm
 
 echo '
 PS1="\[$(tput setab 6)\]\[$(tput setaf 0)\]\w\[$(tput sgr0)\]\[$(tput setaf 6)\]\[$(tput sgr0)\] "
@@ -88,10 +82,17 @@ echo '<?xml version="1.0"?>
 #dconf update
 
 # https://access.redhat.com/documentation/en-us/red_hat_enterprise_linux/7/html/desktop_migration_and_administration_guide/custom-default-values-system-settings
+echo '
+' > /usr/share/glib-2.0/schemas/19_mysettings.gschema.override
+glib-compile-schemas /usr/share/glib-2.0/schemas
+
+mkdir -p /etc/dconf/db/local.d/locks
+echo '
+' > /etc/dconf/db/local.d/locks/00_mylocks
+dconf update
+
 mkdir -p /usr/local/share/gnome-shell/extensions/gnome-shell-improved/
 echo '{
   "uuid": "gnome-shell-improved"
 }' > /usr/local/share/gnome-shell/extensions/gnome-shell-improved/metadata.json
 cp ./extension.js /usr/local/share/gnome-shell/extensions/gnome-shell-improved/
-
-systemctl enable gdm
