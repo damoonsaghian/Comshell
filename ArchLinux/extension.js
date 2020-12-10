9ba3bfa128b5c2264c3dc3015467f547e2e7f17d
Damoon Saghian <damoonsaghian84@gmail.com>
2020-12-11 00:17:59 +0330

    Add files via upload

---
+++ ArchLinux/extension.js
@@ -0,0 +1,416 @@
+function init() {
+const main = imports.ui.main;
+const GObject = imports.gi.GObject;
+const St = imports.gi.St;
+const Clutter = imports.gi.Clutter;
+const Meta = imports.gi.Meta;
+const Shell = imports.gi.Shell;
+
+main.setThemeStylesheet(
+  "/usr/local/share/gnome-shell/extensions/gnome-shell-improved/style.css");
+main.loadTheme();
+
+// https://github.com/eonpatapon/gnome-shell-extension-caffeine
+// https://extensions.gnome.org/extension/672/disable-screen-shield/
+
+// hide notification banners;
+{
+  const bannerBin = main.messageTray._bannerBin;
+  if (bannerBin) bannerBin.hide();
+}
+
+// move status_bar to the bottom;
+{
+  const panelBox = main.layoutManager.panelBox;
+  function movePanelToBottom() {
+    const monitor = main.layoutManager.primaryMonitor;
+    const y = monitor.height - panelBox.height;
+    panelBox.set_position(0, y);
+  }
+  main.layoutManager.connect("monitors-changed", movePanelToBottom);
+  panelBox.connect("notify::height", movePanelToBottom);
+  movePanelToBottom();
+}
+
+main.panel.statusArea.activities.container.connect("show", c => c.hide());
+main.panel.statusArea.activities.container.hide();
+main.panel.statusArea.appMenu.container.connect("show", c => c.hide());
+main.panel.statusArea.appMenu.container.hide();
+main.panel.statusArea.dateMenu.container.connect("show", c => c.hide());
+main.panel.statusArea.dateMenu.container.hide();
+main.panel.statusArea.aggregateMenu.container.connect("show", c => c.hide());
+main.panel.statusArea.aggregateMenu.container.hide();
+
+// right side of status_bar;
+{
+  const rightButton = new imports.ui.panelMenu.Button(0.0, null, true);
+  main.panel.addToStatusArea("status_right", rightButton, 0, "right");
+  const rightBox = new St.BoxLayout({ style_class: "panel-status-indicators-box" });
+  rightButton.add_child(rightBox);
+
+  const screencast = main.panel.statusArea.aggregateMenu._screencast;
+  if (screencast && screencast._indicator) {
+    screencast.indicators.remove_child(screencast._indicator);
+    rightBox.add_child(screencast._indicator);
+    screencast._sync();
+  }
+  /*
+  // since screencast implementation is simple, an alternative method is:
+  const screencastIcon = new St.Icon({ style_class: "system-status-icon" });
+  rightBox.add_child(screencastIcon);
+  screencastIcon.icon_name = "media-record-symbolic";
+  screencastIcon.add_style_class_name("screencast-indicator");
+  main.screencastService.connect("updated",
+    () => screencastIcon.visible = main.screencastService.isRecording
+  );
+  */
+
+  // install ArchLinux updates as scheduled;
+  // https://www.techrapid.uk/2017/04/automatically-update-arch-linux-with-systemd.html
+  // https://wiki.archlinux.org/index.php/Systemd/Timers
+  // a red indicator appears to notify the user that for system to update, it needs a reboot;
+  // https://github.com/RaphaelRochet/arch-update
+
+  const remoteAccess = main.panel.statusArea.aggregateMenu._remoteAccess;
+  if (remoteAccess && remoteAccess._indicator) {
+    remoteAccess._ensureControls();
+    remoteAccess.indicators.remove_child(remoteAccess._indicator);
+    rightBox.add_child(remoteAccess._indicator);
+    remoteAccess._sync();
+  }
+
+  const network = main.panel.statusArea.aggregateMenu._network;
+  if (network && network._primaryIndicator) {
+    network.indicators.remove_child(network._primaryIndicator);
+    rightBox.add_child(network._primaryIndicator);
+    if (network._vpnIndicator) {
+      network.indicators.remove_child(network._vpnIndicator);
+      rightBox.add_child(network._vpnIndicator);
+    }
+  }
+
+  // https://github.com/hedayaty/NetSpeed/blob/master/net_speed.js
+  // https://github.com/hedayaty/NetSpeed/blob/master/net_speed_status_icon.js
+
+  const rfkillIcon = new St.Icon({ style_class: "system-status-icon" });
+  rightBox.add_child(rfkillIcon);
+  rfkillIcon.icon_name = "network-wireless-disabled-symbolic";
+  const rfkillManager = imports.ui.status.rfkill.getRfkillManager();
+  rfkillIcon.visible = rfkillManager.airplaneMode;
+  rfkillManager.connect("airplane-mode-changed",
+    () => rfkillIcon.visible = rfkillManager.airplaneMode
+  );
+
+  const volume = main.panel.statusArea.aggregateMenu._volume;
+  if (volume && volume._primaryIndicator) {
+    volume.indicators.remove_child(volume._primaryIndicator);
+    rightBox.add_child(volume._primaryIndicator);
+    if (volume._inputIndicator) {
+      volume.indicators.remove_child(volume._inputIndicator);
+      rightBox.add_child(volume._inputIndicator);
+      volume._inputIndicator.visible = true;
+    }
+  }
+
+  const power = main.panel.statusArea.aggregateMenu._power;
+  if (power && power._indicator) {
+    power.indicators.remove_child(power._indicator);
+    rightBox.add_child(power._indicator);
+    if (power._percentageLabel) {
+      power.indicators.remove_child(power._percentageLabel);
+      rightBox.add_child(power._percentageLabel);
+    }
+    // over_write "_sync" method, to hide the power icon, if there's no battery;
+    if (power._sync && power._proxy) {
+      power._sync = function() {
+        imports.ui.status.power.Indicator.prototype._sync.call(this);
+        if (!this._proxy.IsPresent) this._indicator.hide();
+      };
+      power._sync();
+    }
+  }
+
+  // https://github.com/Ory0n/Resource_Monitor/
+  // https://github.com/corecoding/Vitals
+  // https://github.com/paradoxxxzero/gnome-shell-system-monitor-applet
+
+  const location = main.panel.statusArea.aggregateMenu._location;
+  if (location && location._indicator) {
+    location.indicators.remove_child(location._indicator);
+    rightBox.add_child(location._indicator);
+    location._syncIndicator();
+  }
+
+  const dateTimeLabel = new St.Label({ y_align: Clutter.ActorAlign.CENTER });
+  rightBox.add_child(dateTimeLabel);
+  const updateClock = () => {
+    const now = imports.gi.GLib.DateTime.new_now_local();
+    const nowFormated = now ? now.format("%F %a %p %I:%M") : "";
+    // https://github.com/omid/Persian-Calendar-for-Gnome-Shell/blob/master/PersianCalendar%40oxygenws.com/PersianDate.js
+    dateTimeLabel.set_text(nowFormated);
+  };
+  updateClock();
+  const wallClock = main.panel.statusArea.dateMenu._clock;
+  if (wallClock) wallClock.connect("notify::clock", updateClock);
+}
+
+// left side of status_bar;
+{
+  const leftButton = new imports.ui.panelMenu.Button(0.0, null, true);
+  main.panel.addToStatusArea("status_left", leftButton, 0, "left");
+  const leftBox = new St.BoxLayout({ style_class: "panel-status-indicators-box" });
+  leftButton.add_child(leftBox);
+
+  // https://github.com/home-sweet-gnome/dash-to-panel/blob/master/appIcons.js
+  // https://gjs-docs.gnome.org/clutter7~7_api/
+  // https://gjs-docs.gnome.org/st10~1.0_api/
+  // https://gjs-docs.gnome.org/meta7~7_api/
+  // https://gjs-docs.gnome.org/shell01~0.1_api/
+  // https://gitlab.gnome.org/GNOME/gnome-shell/-/tree/master/js
+/*
+  const windowList = new St.Widget({
+    layout_manager: new Clutter.BoxLayout({ homogeneous: true , spacing: 8}),
+    x_align: Clutter.ActorAlign.START,
+    x_expand: true,
+    y_expand: true,
+  });
+  leftBox.add_child(windowList);
+
+  const AppButton = GObject.registerClass(
+  class AppButton extends St.Button {
+    _init(app) {
+      super._init({
+        can_focus: true,
+        x_expand: true,
+        button_mask: St.ButtonMask.ONE | St.ButtonMask.THREE,
+      });
+
+      this.connect('notify::allocation', this._updateIconGeometry.bind(this));
+      this.connect('destroy', this._onDestroy.bind(this));
+
+      this.app = app;
+
+      this._multiWindowTitle = new St.BoxLayout({
+        x_expand: true,
+      });
+      this.set_child(this._multiWindowTitle);
+
+      this._icon = new St.Bin({
+        style_class: 'window-button-icon',
+        child: app.create_icon_texture(ICON_TEXTURE_SIZE),
+      });
+      this._multiWindowTitle.add(this._icon);
+
+      let label = new St.Label({
+        text: app.get_name(),
+        y_align: Clutter.ActorAlign.CENTER,
+      });
+      this._multiWindowTitle.add(label);
+      this._multiWindowTitle.label_actor = label;
+
+      this._textureCache = St.TextureCache.get_default();
+      this._iconThemeChangedId =
+        this._textureCache.connect('icon-theme-changed', () => {
+          this._icon.child = app.create_icon_texture(24);
+        });
+
+      this._windowsChangedId = this.app.connect(
+        'windows-changed', this._windowsChanged.bind(this));
+      this._windowsChanged();
+
+      this._windowTracker = Shell.WindowTracker.get_default();
+      this._notifyFocusId = this._windowTracker.connect(
+        'notify::focus-app', this._updateStyle.bind(this));
+      this._updateStyle();
+    }
+
+    activate() {
+      let windows = this.getWindowList();
+      windows[0].activate(global.get_current_time());
+      if (!windows.length === 1) {
+        for (let i = 0; i < windows.length; i++) {
+          let windowTitle = new WindowTitle(windows[i]);
+        }
+      }
+    }
+
+    _updateStyle() {
+      if (this._isFocused())
+        this.add_style_class_name('focused');
+      else
+        this.remove_style_class_name('focused');
+    }
+
+    _isWindowVisible(window) {
+      let workspace = global.workspace_manager.get_active_workspace();
+
+      return !window.skip_taskbar;
+    }
+
+    _getIconGeometry() {
+      let rect = new Meta.Rectangle();
+
+      [rect.x, rect.y] = this.get_transformed_position();
+      [rect.width, rect.height] = this.get_transformed_size();
+
+      return rect;
+    }
+
+    //
+
+    _isFocused() {
+      return this._windowTracker.focus_app === this.app;
+    }
+
+    _updateIconGeometry() {
+      let rect = this._getIconGeometry();
+
+      let windows = this.app.get_windows();
+      windows.forEach(w => w.set_icon_geometry(rect));
+    }
+
+    getWindowList() {
+      return this.app.get_windows().filter(win => this._isWindowVisible(win));
+    }
+
+    _windowsChanged() {
+      if (this._windowTitle) {
+        this.metaWindow = null;
+        this._windowTitle = null;
+      }
+      this.label_actor = this._multiWindowTitle.label_actor;
+    }
+
+    _onMenuActivate(menu, child) {
+      child._window.activate(global.get_current_time());
+    }
+
+    _onDestroy() {
+      this._textureCache.disconnect(this._iconThemeChangedId);
+      this._windowTracker.disconnect(this._notifyFocusId);
+      this.app.disconnect(this._windowsChangedId);
+    }
+  });
+
+  const onWindowAdded = (_ws, win) => {
+    if (win.get_monitor() !== 0) return;
+
+    const app = Shell.WindowTracker.get_default().get_window_app(win);
+    const children = windowList.get_children();
+    const child = children.find(c => c.app === app);
+
+    // activate app
+
+    if (child) {
+      // move app button to the first place;
+      windowList.set_child_at_index(child, 0);
+      // add an indicator to the app button;
+    } else {
+      const button = new AppButton(app);
+      windowList.insert_child_at_index(button, 0);
+      // move app button to the first place;
+    }
+  };
+
+  const onWindowRemoved = (_ws, win) => {
+    if (win.get_monitor() !== 0) return;
+
+    const app = Shell.WindowTracker.get_default().get_window_app(win);
+    const children = windowList.get_children();
+    const child = children.find(c => c.app === app);
+
+    // if child exists and without indicators
+    //child.destroy();
+    // else remove one indicator from the app button;
+  };
+
+  const workspace_manager = global.workspace_manager;
+  const connect_workspace = (i) => {
+    const workspace = workspace_manager.get_workspace_by_index(i);
+    workspace.connect("window-added", onWindowAdded);
+    workspace.connect("window-removed", onWindowRemoved);
+    // when focused window changes
+  };
+  for (let i = 0; i < workspace_manager.n_workspaces; i++) {
+    connect_workspace(i);
+  }
+  workspace_manager.connect("workspace-added", (_wsm, i) => connect_workspace(i));
+
+  const windows = global.get_window_actors().sort((w1, w2) => {
+    return w1.metaWindow.get_stable_sequence() - w2.metaWindow.get_stable_sequence();
+  });
+  for (let i = 0; i < windows.length; i++)
+    onWindowAdded(null, windows[i].metaWindow);
+*/
+}
+
+// overview layer
+{
+  const overview = main.overview;
+  const viewSelector = overview.viewSelector;
+  const windowManager = main.wm;
+
+  const toggleOverview = () => {
+    if (overview.isDummy) return;
+    if (overview.visible) overview.hide();
+    else viewSelector.showApps();
+  }
+
+  // toggle overview when toggling apps view;
+  windowManager.removeKeybinding("toggle-application-view");
+  windowManager.addKeybinding(
+    "toggle-application-view",
+    new imports.gi.Gio.Settings({ schema_id: imports.ui.windowManager.SHELL_KEYBINDINGS_SCHEMA }),
+    Meta.KeyBindingFlags.IGNORE_AUTOREPEAT,
+    Shell.ActionMode.NORMAL | Shell.ActionMode.OVERVIEW,
+    toggleOverview
+  );
+
+  // toggle overview when clicking on an empty area on the panel;
+  main.panel.connect("button-press-event", toggleOverview);
+
+  // hide overview when pressing "alt-tab";
+  windowManager.setCustomKeybindingHandler(
+    "switch-applications",
+    Shell.ActionMode.NORMAL | Shell.ActionMode.OVERVIEW,
+    (display, win, binding) => {
+      if (overview.visible) { overview.hide(); return; }
+      windowManager._startSwitcher(display, win, binding);
+    }
+  );
+
+  // close overview by pressing "esc" key only once;
+  // based on _onStageKeyPress function from:
+  //   https://gitlab.gnome.org/GNOME/gnome-shell/-/blob/master/js/ui/viewSelector.js
+  viewSelector._onStageKeyPress = function (actor, event) {
+    if (main.modalCount > 1)
+      return Clutter.EVENT_PROPAGATE;
+
+    let symbol = event.get_key_symbol();
+
+    if (symbol === Clutter.KEY_Escape) {
+      if (this._searchActive)
+        this.reset();
+      else
+        main.overview.hide();
+      return Clutter.EVENT_STOP;
+    } else if (this._shouldTriggerSearch(symbol)) {
+        this.startSearch(event);
+    } else if (!this._searchActive && !global.stage.key_focus) {
+      if (symbol === Clutter.KEY_Tab || symbol === Clutter.KEY_Down) {
+        this._activePage.navigate_focus(null, St.DirectionType.TAB_FORWARD, false);
+        return Clutter.EVENT_STOP;
+      } else if (symbol === Clutter.KEY_ISO_Left_Tab) {
+        this._activePage.navigate_focus(null, St.DirectionType.TAB_BACKWARD, false);
+        return Clutter.EVENT_STOP;
+      }
+    }
+    return Clutter.EVENT_PROPAGATE;
+  }
+
+  // hide dash;
+  overview.connect("showing", () => overview.dash.hide());
+}
+
+return { enable: () => {}, disable: () => {} };
+}
--- ArchLinux/install.sh
+++ ArchLinux/install.sh
@@ -5,8 +5,7 @@ printf 'LANG=en_US.UTF-8\n' > /etc/locale.conf
 pacman -S grub intel-ucode amd-ucode linux linux-firmware \
   btrfs-progs e2fsprogs dosfstools udisks2 networkmanager pulseaudio-alsa \
   nano man-db unzip ttf-hack noto-fonts materia-gtk-theme \
-  sway swayidle gjs gtk-layer-shell alacritty \
-  xorg-server-xwayland polkit-gnome
+  gnome-shell gdm gvfs gnome-terminal
 
 printf '\nGRUB_TIMEOUT=0\nGRUB_DISABLE_OS_PROBER=true\n' >> /etc/default/grub
 printf '\nset superusers=""\n' >> /etc/grub.d/40_custom
@@ -30,15 +29,7 @@ Exec = /usr/bin/grub-mkstandalone -O x86_64-efi -o "/boot/efi/EFI/BOOT/BOOTX64.E
 systemctl enable systemd-timesyncd
 systemctl enable NetworkManager
 systemctl enable NetworkManager-dispatcher
-
-# automatic time_zone:
-echo '
-#!/bin/sh
-if [ "$2" = "connectivity-change" ] && [ -z "$VPN_IP_IFACE" ]; then
-  timedatectl set-timezone "$(curl -sf http://ip-api.com/line/?fields=timezone)"
-fi
-' > /etc/NetworkManager/dispatcher.d/09-timezone
-chmod 755 /etc/NetworkManager/dispatcher.d/09-timezone
+systemctl enable gdm
 
 # to customize dconf default values:
 mkdir -p /etc/dconf/profile
@@ -51,15 +42,70 @@ echo "[org/gnome/system/location]
 enabled=true
 [org/gnome/desktop/datetime]
 automatic-timezone=true
+[org/gnome/desktop/notifications]
+show-banners=false
+show-in-lock-screen=false
+[org/gnome/desktop/background]
+primary-color='#222222'
+secondary-color='#222222'
 [org/gnome/desktop/interface]
-document-font-name='sans 10.5'
-monospace-font-name='monospace 10.5'
-font-name='sans 10.5'
-gtk-theme='Materia-light-compact'
 overlay-scrolling=false
+document-font-name='sans 10.5'
+font-name='sans 10.5'
+monospace-font-name='monospace 10.5'
+gtk-theme='Materia-light-compact'
 cursor-blink-timeout=1000
+enable-hot-corners=false
+[org/gnome/mutter]
+center-new-windows=true
+[org/gnome/desktop/wm/preferences]
+button-layout=''
+[org/gnome/desktop/wm/keybindings]
+close=['<Alt>Escape']
+toggle-maximized=['<Alt><Shift>Space']
+activate-window-menu=['']
+[org/gnome/shell/keybindings]
+toggle-application-view=['<Alt>Space', '<Super>a']
+[org/gnome/shell]
+disable-extension-version-validation=true
+enabled-extensions=['gnome-shell-improved']
 " > /etc/dconf/db/local.d/00-mykeyfile
 dconf update
+
+mkdir -p /usr/local/share/gnome-shell/extensions/gnome-shell-improved/
+echo '{
+  "uuid": "gnome-shell-improved",
+  "name": "GnomeShellImproved",
+  "description": "GnomeShell improved",
+  "shell-version": []
+}' > /usr/local/share/gnome-shell/extensions/gnome-shell-improved/metadata.json
+echo 'stage {
+  font-family: sans;
+  font-size: 10.5pt;
+  font-weight: normal;
+  color: #ffffff;
+}
+#panel {
+  height: 18px;
+}
+#panel .panel-button {
+  -natural-hpadding: 4px;
+  -minimum-hpadding: 4px;
+  margin: 1px 0px 0px 0px;
+  font-family: monospace;
+  font-size: 10.5pt;
+  font-weight: normal;
+  color: #ffffff;
+}
+.panel-status-indicators-box {
+  spacing: 8px;
+}
+.window-button-icon {
+  height: 16px;
+  padding: 0px 2px 0 0;
+}
+' > /usr/local/share/gnome-shell/extensions/gnome-shell-improved/style.css
+cp ./extension.js /usr/local/share/gnome-shell/extensions/gnome-shell-improved/
 
 echo '<?xml version="1.0"?>
 <!DOCTYPE fontconfig SYSTEM "fonts.dtd">
@@ -118,6 +164,10 @@ fi
 mkdir -p /etc/skel/.config/sway
 cp ./sway /etc/skel/.config/sway/config
 cp ./swayshell.js /usr/local/share/
+
+printf '[Desktop Entry]\nNoDisplay=true' |
+tee /etc/skel/.local/share/applications/{avahi-discover,bssh,bvnc,qv4l2,qvidcap,lstopo}.desktop >
+  /dev/null
 
 mkdir -p /etc/skel/.config/alacritty
 echo 'window:
